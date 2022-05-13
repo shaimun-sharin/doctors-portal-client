64245d72e7a2957e6eb91bcc9ba8964c4e432f31
shaimun-sharin <shaimunsharin@gmail.com>
2022-05-13 22:25:00 +0600

    home page done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,12 +2,20 @@ import React from "react";
 import Info from "../../Info/Info";
 import Footer from "../../Shared/Footer/Footer";
 import Banner from "../Banner/Banner";
+import Contact from "../Contact";
+import MakeAppointment from "../MakeAppointment";
+import Testimonials from "../Testimonials";
+import Services from "./Services";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Info></Info>
+      <Services></Services>
+      <MakeAppointment></MakeAppointment>
+      <Testimonials></Testimonials>
+      <Contact></Contact>
       <Footer></Footer>
     </div>
   );
--- src/Pages/Info/Info.js
+++ src/Pages/Info/Info.js
@@ -6,8 +6,22 @@ import phone from "../../../src/assets/icons/phone.svg";
 
 const Info = () => {
   return (
-    <div>
-      <InfoCard img={clock}></InfoCard>
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+      <InfoCard
+        cardTitle="Opening Hours"
+        bgClass="bg-gradient-to-r from-secondary to-primary"
+        img={clock}
+      ></InfoCard>
+      <InfoCard
+        cardTitle="Our Locations"
+        bgClass="bg-neutral"
+        img={marker}
+      ></InfoCard>
+      <InfoCard
+        cardTitle="Contact Us"
+        bgClass="bg-gradient-to-r from-secondary to-primary"
+        img={phone}
+      ></InfoCard>
     </div>
   );
 };
