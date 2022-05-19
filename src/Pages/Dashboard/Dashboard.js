f40f61240f2920ef506df08247fbdc15de090f9d
shaimun-sharin <shaimunsharin@gmail.com>
2022-05-19 23:15:06 +0600

    new component

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link, Outlet } from "react-router-dom";
+import auth from "../../firebase.init";
+import useAdmin from "../../hooks/useAdmin";
 
 const Dashboard = () => {
+  const [user] = useAuthState(auth);
+  const [admin] = useAdmin(user);
   return (
     <div class="drawer drawer-mobile">
       <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
@@ -23,9 +28,11 @@ const Dashboard = () => {
           <li>
             <Link to="/dashboard/history">My History</Link>
           </li>
-          <li>
-            <Link to="/dashboard/users">All users</Link>
-          </li>
+          {admin && (
+            <li>
+              <Link to="/dashboard/users">All users</Link>
+            </li>
+          )}
         </ul>
       </div>
     </div>
--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -10,10 +10,17 @@ const UserRow = ({ user, refetch }) => {
         authorization: `Bearer ${localStorage.getItem("accessToken")}`,
       },
     })
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.status === 403) {
+          toast.error("Failed To Make An Admin");
+        }
+        return res.json();
+      })
       .then((data) => {
-        refetch();
-        toast.success("SuccessFully Made an Admin");
+        if (data.modifiedCount > 0) {
+          refetch();
+          toast.success("SuccessFully Made an Admin");
+        }
       });
   };
   return (
