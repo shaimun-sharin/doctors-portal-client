254f4454b88342d5f24ad483e59b0785facb39b4
shaimun-sharin <shaimunsharin@gmail.com>
2022-05-20 13:56:10 +0600

    new coponent created and successfully deleted doctor

---
+++ src/Pages/Dashboard/DoctorRow.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { toast } from "react-toastify";
+
+const DoctorRow = ({ doctor, refetch, index }) => {
+  const { name, speciality, img, email } = doctor;
+  const handleDelete = (email) => {
+    fetch(`http://localhost:5000/doctor/${email}`, {
+      method: "DELETE",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount) {
+          toast.success(`doctor ${name} is deleted`);
+          refetch();
+        }
+      });
+  };
+  return (
+    <tr>
+      <th>{index + 1}</th>
+      <td>
+        <div class="avatar">
+          <div class="w-8 rounded-full">
+            <img src={img} alt={name} />
+          </div>
+        </div>
+      </td>
+      <td>{name}</td>
+      <td>{speciality}</td>
+      <td>
+        <button
+          onClick={() => handleDelete(email)}
+          class="btn btn-xs btn-error"
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default DoctorRow;
--- src/Pages/Dashboard/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { useQuery } from "react-query";
 import Loading from "../Shared/Loading";
+import DoctorRow from "./DoctorRow";
 
 const ManageDoctors = () => {
-  const { data: doctors, isLoading } = useQuery("doctors", () =>
+  const {
+    data: doctors,
+    isLoading,
+    refetch,
+  } = useQuery("doctors", () =>
     fetch("http://localhost:5000/doctor", {
       headers: {
         authorization: `Bearer ${localStorage.getItem("accessToken")}`,
@@ -16,6 +21,29 @@ const ManageDoctors = () => {
   return (
     <div>
       <h1 className="text-3xl"> All doctors:{doctors.length}</h1>
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Avatar</th>
+              <th>Name</th>
+              <th>Speciality</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {doctors.map((doctor, index) => (
+              <DoctorRow
+                key={doctor._id}
+                doctor={doctor}
+                refetch={refetch}
+                index={index}
+              ></DoctorRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
